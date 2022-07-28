927a9a752972462ac947b9f3e20212f2a8be4680
Cody Olsen <stipsan@gmail.com>
2022-07-28 20:40:18 +0200

    refactor: ThemeProviderFixer no longer needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import {type Rule, createPlugin} from 'sanity'
+import {createPlugin} from 'sanity'
 
 import Input from './components/Input'
-import ThemeProviderFixer from './components/ThemeProviderFixer'
 import muxVideo from './schema/mux.video'
 import videoAsset from './schema/mux.videoAsset'
 import {isMuxInputProps} from './util/asserters'
@@ -36,30 +35,6 @@ export const muxInput = createPlugin<Config>((userConfig) => {
     form: {
       renderInput(props, next) {
         if (isMuxInputProps(props)) {
-          if (process.env.NODE_ENV !== 'production') {
-            // @TODO this seems to only be a risk when using yalc symlinking in dev mode, investigate a way to reliably reproduce and adress this error
-            /**
-             * How to reproduce the issue that makes this workaround necessary:
-              import {Text} from '@sanity/ui'
-              import {render} from 'react-dom'
-
-              const node = document.createElement('div')
-              document.body.append(node)
-
-              render(
-                <ThemeProviderFixer>
-                  <Text>Hi!</Text>
-                </ThemeProviderFixer>,
-                node
-              )
-             */
-            return (
-              <ThemeProviderFixer>
-                <Input config={config} {...props} />
-              </ThemeProviderFixer>
-            )
-          }
-
           return <Input config={config} {...props} />
         }
         return next(props)
