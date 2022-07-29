e113b1d4d44b94e98d3fe81d0a8e09bf65b6e194
Cody Olsen <stipsan@gmail.com>
2022-07-29 19:16:32 +0200

    fix: code split around issues in CJS

--- src/util/generateJwt.ts
+++ src/util/generateJwt.ts
@@ -34,11 +34,13 @@ export function generateJwt<T extends Audience>(
   const {default: sign}: {default: typeof import('jsonwebtoken-esm/sign')['default']} =
     suspend(() => {
       // eslint-disable-next-line @typescript-eslint/no-shadow
+      // This whole skypack thing is an extreme, temporary, measure to get around Parcel bugs with code splitting, dynamic import, and CJS + ESM interop
+      // Otherwise known as the unholy trinity of reasons I might consider going back to doing only design work
       // const {default: sign} = await import('jsonwebtoken-esm/sign')
       // eslint-disable-next-line @typescript-eslint/no-shadow
       return import(
         /* webpackIgnore: true */
-        // @ts-expect-error -- TS don't like HRL classes in dynamic imports even though it's valid
+        // @ts-expect-error -- TS don't like URL classes in dynamic imports even though it's valid
         new URL(
           'https://cdn.skypack.dev/pin/jsonwebtoken-esm@v1.0.3-p8N0qksX2r9oYz3jfz0a/mode=imports,min/optimized/jsonwebtoken-esm/sign.js'
         )
