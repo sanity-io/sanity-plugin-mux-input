4111127b463a80fc13c3f91ca4b0be36936543f1
Cody Olsen <stipsan@gmail.com>
2022-09-14 16:05:36 +0200

    chore: sync with `main`

---
+++ ignored-build-step.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Next.js builds are only supported for the Studio v3 version of the plugin
+# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+echo "🛑 - Build cancelled"
+exit 0;
