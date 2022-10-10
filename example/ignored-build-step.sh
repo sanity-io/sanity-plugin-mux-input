5c8c5949d2cd4c13a72bf601d1b372747da855c4
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-10-10 17:37:11 +0200

    chore(ci): add ignored build step script

---
+++ example/ignored-build-step.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Codesandbox builds are only needed on the main branch, not on the studio-v3 branch
+# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel
+echo "✅ - Build can proceed"
+exit 1;
