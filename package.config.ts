f4e3ab230400dc17da4483a473be68061e798ca7
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-01-14 15:19:05 +0100

    fix: workaround `hls.js` bug affecting `vite`

--- package.config.ts
+++ package.config.ts
@@ -1,9 +1,13 @@
 import {defineConfig} from '@sanity/pkg-utils'
+import alias from '@rollup/plugin-alias'
 
 export default defineConfig({
-  legacyExports: true,
+  // @TODO stop pre-bundling @mux/mux-video-react once this is fixed https://github.com/video-dev/hls.js/issues/5146
+  external: (externals) => externals.filter((e) => e !== '@mux/mux-video-react'),
   dist: 'lib',
   tsconfig: 'tsconfig.lib.json',
+  // @TODO stop using the alias plugin, uninstall it and rollup once this is fixed https://github.com/video-dev/hls.js/issues/5146
+  rollup: {plugins: [alias({entries: [{find: 'hls.js', replacement: 'hls.js/dist/hls.min'}]})]},
 
   // Remove this block to enable strict export validation
   extract: {
