0d2b8e427a1e6e97d131aa8e6ce83384f856a516
Cody Olsen <stipsan@gmail.com>
2023-02-27 23:27:32 +0100

    fix(hls.js): remove vite workaround (#219)

--- package.config.ts
+++ package.config.ts
@@ -1,27 +1,10 @@
 import {defineConfig} from '@sanity/pkg-utils'
-import alias from '@rollup/plugin-alias'
-import {createRequire} from 'node:module'
-
-const require = createRequire(import.meta.url)
 
 export default defineConfig({
   // @TODO stop pre-bundling @mux/mux-video-react once this is fixed https://github.com/video-dev/hls.js/issues/5146
   external: (externals) => externals.filter((e) => e !== '@mux/mux-video-react'),
   dist: 'lib',
   tsconfig: 'tsconfig.lib.json',
-  // @TODO stop using the alias plugin, uninstall it and rollup once this is fixed https://github.com/video-dev/hls.js/issues/5146
-  rollup: {
-    plugins: [
-      alias({
-        entries: [
-          {
-            find: /(?<!\/)hls\.js(?!\/)/,
-            replacement: require.resolve('hls.js/dist/hls.min'),
-          },
-        ],
-      }),
-    ],
-  },
 
   // Remove this block to enable strict export validation
   extract: {
