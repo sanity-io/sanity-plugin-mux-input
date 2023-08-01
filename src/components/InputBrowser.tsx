c491ebc342aa1a2b1c2d6f240db90af69d6aa5fc
Henrique Doro <opensource@hdoro.dev>
2023-08-01 07:49:14 -0300

    fix: don't force InputBrowser's color scheme

--- src/components/InputBrowser.tsx
+++ src/components/InputBrowser.tsx
@@ -11,14 +11,7 @@ export default function InputBrowser({setDialogState, asset, onChange}: Props) {
   const id = `InputBrowser${useId()}`
   const handleClose = useCallback(() => setDialogState(false), [setDialogState])
   return (
-    <Dialog
-      scheme="dark"
-      __unstable_autoFocus
-      header="Select video"
-      id={id}
-      onClose={handleClose}
-      width={2}
-    >
+    <Dialog __unstable_autoFocus header="Select video" id={id} onClose={handleClose} width={2}>
       <SelectAsset asset={asset} onChange={onChange} setDialogState={setDialogState} />
     </Dialog>
   )
