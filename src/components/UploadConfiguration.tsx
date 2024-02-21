12bc2efd96698acb3a84ada42303d65946d7ad42
Henrique Doro <opensource@hdoro.dev>
2024-02-21 14:43:07 -0300

    docs: documentation of new options

--- src/components/UploadConfiguration.tsx
+++ src/components/UploadConfiguration.tsx
@@ -1,5 +1,5 @@
 import {CloseIcon, UploadIcon} from '@sanity/icons'
-import {Button, Card, Checkbox, Dialog, Flex, Radio, Stack, Text, TextInput} from '@sanity/ui'
+import {Button, Card, Checkbox, Dialog, Flex, Grid, Radio, Stack, Text, TextInput} from '@sanity/ui'
 import {useId, useState} from 'react'
 import {ENCODING_TIERS, PluginConfig, Secrets, UploadConfig} from '../util/types'
 import FormField from './FormField'
@@ -59,7 +59,7 @@ export default function UploadConfiguration({
         </Card>
       }
     >
-      <Stack space={4} padding={2}>
+      <Stack space={4} paddingY={2} paddingX={4} as="form">
         {videoUrl && (
           <Flex gap={2} align="center">
             <Card
@@ -77,45 +77,37 @@ export default function UploadConfiguration({
             {file?.name && <Text muted>{file?.name}</Text>}
           </Flex>
         )}
-        <Stack
-          space={4}
-          style={{
-            position: 'sticky',
-            top: '1em',
-          }}
-          flex={1}
-          as="form"
-          sizing="border"
-        >
-          <FormField title="Video title" inputId={titleId}>
-            <TextInput
-              id={titleId}
-              onChange={(e) => modifyProperty({title: e.currentTarget.value})}
-              type="text"
-              value={config.title || ''}
-              tabIndex={0}
+        <FormField title="Video title" inputId={titleId}>
+          <TextInput
+            id={titleId}
+            onChange={(e) => modifyProperty({title: e.currentTarget.value})}
+            type="text"
+            value={config.title || ''}
+            tabIndex={0}
+          />
+        </FormField>
+        {secrets.enableSignedUrls && (
+          <Flex align="center" gap={2}>
+            <Checkbox
+              id={`${id}--signed`}
+              style={{display: 'block'}}
+              name="signed"
+              required
+              checked={config.signed}
+              onChange={(e) => {
+                modifyProperty({
+                  signed: e.currentTarget.checked,
+                })
+              }}
             />
-          </FormField>
-          {secrets.enableSignedUrls && (
-            <Flex align="center" gap={2}>
-              <Checkbox
-                id={`${id}--signed`}
-                style={{display: 'block'}}
-                name="signed"
-                required
-                checked={config.signed}
-                onChange={(e) => {
-                  modifyProperty({
-                    signed: e.currentTarget.checked,
-                  })
-                }}
-              />
-              <Text>
-                <label htmlFor={`${id}--signed`}>Signed playback URL</label>
-              </Text>
-            </Flex>
-          )}
-          <fieldset style={{all: 'unset'}}>
+            <Text>
+              <label htmlFor={`${id}--signed`}>Signed playback URL</label>
+            </Text>
+          </Flex>
+        )}
+
+        <Grid gap={4} style={{position: 'relative', alignItems: 'flex-start'}} columns={[1, 2]}>
+          <fieldset style={{all: 'unset', position: 'sticky', top: '1rem'}}>
             <Stack space={2}>
               <Text as="legend" size={1} weight="semibold">
                 Encoding tier
@@ -144,68 +136,71 @@ export default function UploadConfiguration({
               })}
             </Stack>
           </fieldset>
+          {config.encoding_tier === 'smart' && (
+            <Stack space={4} style={{position: 'sticky', top: '1rem'}}>
+              {pluginConfig.max_resolution_tier !== '1080p' && (
+                <fieldset style={{all: 'unset'}}>
+                  <Stack space={2}>
+                    <Text as="legend" size={1} weight="semibold">
+                      Max resolution
+                    </Text>
+                    {getResolutionOptions(pluginConfig).map(({value, label}) => {
+                      const inputId = `${id}--max_resolution-${value}`
+                      return (
+                        <Flex key={value} align="center" gap={2}>
+                          <Radio
+                            checked={config.max_resolution_tier === value}
+                            name="max_resolution"
+                            onChange={(e) => {
+                              modifyProperty({
+                                max_resolution_tier: e.currentTarget
+                                  .value as UploadConfig['max_resolution_tier'],
+                              })
+                            }}
+                            value={value}
+                            id={inputId}
+                          />
+                          <Text as="label" htmlFor={inputId}>
+                            {label}
+                          </Text>
+                        </Flex>
+                      )
+                    })}
+                  </Stack>
+                </fieldset>
+              )}
 
-          {config.encoding_tier === 'smart' && pluginConfig.mp4_support !== 'none' && (
-            <Flex align="center" gap={2}>
-              <Checkbox
-                id={`${id}--mp4_support`}
-                style={{display: 'block'}}
-                name="mp4_support"
-                required
-                checked={config.mp4_support === 'standard'}
-                onChange={(e) => {
-                  modifyProperty({
-                    mp4_support: e.currentTarget.checked ? 'standard' : 'none',
-                  })
-                }}
-              />
-              <Text>
-                <label htmlFor={`${id}--mp4_support`}>MP4 support (allow downloading)</label>
-              </Text>
-            </Flex>
+              {pluginConfig.mp4_support !== 'none' && (
+                <Flex align="center" gap={2}>
+                  <Checkbox
+                    id={`${id}--mp4_support`}
+                    style={{display: 'block'}}
+                    name="mp4_support"
+                    required
+                    checked={config.mp4_support === 'standard'}
+                    onChange={(e) => {
+                      modifyProperty({
+                        mp4_support: e.currentTarget.checked ? 'standard' : 'none',
+                      })
+                    }}
+                  />
+                  <Text>
+                    <label htmlFor={`${id}--mp4_support`}>MP4 support (allow downloading)</label>
+                  </Text>
+                </Flex>
+              )}
+            </Stack>
           )}
+        </Grid>
 
-          {config.encoding_tier === 'smart' && pluginConfig.max_resolution_tier !== '1080p' && (
-            <fieldset style={{all: 'unset'}}>
-              <Stack space={2}>
-                <Text as="legend" size={1} weight="semibold">
-                  Max resolution
-                </Text>
-                {getResolutionOptions(pluginConfig).map(({value, label}) => {
-                  const inputId = `${id}--max_resolution-${value}`
-                  return (
-                    <Flex key={value} align="center" gap={2}>
-                      <Radio
-                        checked={config.max_resolution_tier === value}
-                        name="max_resolution"
-                        onChange={(e) => {
-                          modifyProperty({
-                            max_resolution_tier: e.currentTarget
-                              .value as UploadConfig['max_resolution_tier'],
-                          })
-                        }}
-                        value={value}
-                        id={inputId}
-                      />
-                      <Text as="label" htmlFor={inputId}>
-                        {label}
-                      </Text>
-                    </Flex>
-                  )
-                })}
-              </Stack>
-            </fieldset>
-          )}
-
-          <TextTracksEditor
-            tracks={config.text_tracks || []}
-            setTracks={(newTracks) => {
-              modifyProperty({
-                text_tracks: newTracks as UploadConfig['text_tracks'],
-              })
-            }}
-          />
-        </Stack>
+        <TextTracksEditor
+          tracks={config.text_tracks || []}
+          setTracks={(newTracks) => {
+            modifyProperty({
+              text_tracks: newTracks as UploadConfig['text_tracks'],
+            })
+          }}
+        />
       </Stack>
     </Dialog>
   )
