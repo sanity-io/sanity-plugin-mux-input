5ec9b4d94506caf4dc24c11f1b0313e753de2555
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-11-14 11:26:15 +0100

    chore: remove unused import
    Signed-off-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- src/components/InputBrowser.tsx
+++ src/components/InputBrowser.tsx
@@ -1,5 +1,5 @@
 import {Dialog} from '@sanity/ui'
-import React, {useCallback, useId} from 'react'
+import {useCallback, useId} from 'react'
 import {styled} from 'styled-components'
 
 import type {SetDialogState} from '../hooks/useDialogState'
