86f1c3b509c2a8898367639716141da4c80a2da9
Santiago Puppo <spuppo@mux.com>
2025-10-28 11:51:50 -0300

    Renamed in-code references of encoding_tier into video_quality, keeping in mind compatibility.

--- src/_exports/index.ts
+++ src/_exports/index.ts
@@ -8,7 +8,7 @@ export type {VideoAssetDocument} from '../util/types'
 
 export const defaultConfig: PluginConfig = {
   mp4_support: 'none',
-  encoding_tier: 'smart',
+  video_quality: 'plus',
   max_resolution_tier: '1080p',
   normalize_audio: false,
   defaultSigned: false,
@@ -17,6 +17,15 @@ export const defaultConfig: PluginConfig = {
 }
 
 export const muxInput = definePlugin<Partial<PluginConfig> | void>((userConfig) => {
+  // TODO: Remove this on next major version when we end support for encoding_tier
+  if (typeof userConfig === "object" && 'encoding_tier' in userConfig) {
+    const deprecated_encoding_tier = userConfig.encoding_tier;
+    if (!userConfig.video_quality) {
+      userConfig.video_quality = deprecated_encoding_tier === "baseline" ?
+        "basic" : (deprecated_encoding_tier === "smart") ?
+          "plus" : undefined;
+    }
+  }
   const config: PluginConfig = {...defaultConfig, ...(userConfig || {})}
   return {
     name: 'mux-input',
