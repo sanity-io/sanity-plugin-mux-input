0a0b353efb25a16a9a3c156157e4126a6afc6bac
Lucas Pintos <lucas.pintos@qualabs.com>
2026-01-08 06:36:25 -0300

    feat: add file size and duration validation (#454)

--- src/util/types.ts
+++ src/util/types.ts
@@ -131,6 +131,24 @@ export interface MuxInputConfig {
 
    */
   acceptedMimeTypes?: ('audio/*' | 'video/*')[]
+
+  /**
+   * Maximum file size allowed for video uploads in bytes.
+   * If not specified, no file size validation will be performed.
+   *
+   * @example 1024 * 1024 * 1024 // 1 GB
+   * @defaultValue undefined
+   */
+  maxAssetFileSize?: number
+
+  /**
+   * Maximum video duration allowed in seconds.
+   * If not specified, no duration validation will be performed.
+   *
+   * @example 2 * 60 * 60 // 2 hours
+   * @defaultValue undefined
+   */
+  maxAssetDuration?: number
 }
 
 export interface PluginConfig extends MuxInputConfig {
