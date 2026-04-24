a7d0ce0b784df3ba58efef1b6502b1305eba2970
Renzo Delfino <75499398+R-Delfino95@users.noreply.github.com>
2026-04-24 15:36:30 -0300

    refactor: update PLUGIN_VERSION_QUERY to use process.env.PKG_VERSION instead of package.json import

--- src/util/pluginVersion.ts
+++ src/util/pluginVersion.ts
@@ -1,3 +1,1 @@
-import pkg from '../../package.json'
-
-export const PLUGIN_VERSION_QUERY = {sanityVersion: pkg.version}
+export const PLUGIN_VERSION_QUERY = {sanityVersion: process.env.PKG_VERSION!}
